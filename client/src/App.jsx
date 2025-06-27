7a66c26378f4490d35c000df332ddc2b250ce896
ChazzitUP <hamzarezgui.education@gmail.com>
2025-06-27 14:26:07 +0100

    homepage design and integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
