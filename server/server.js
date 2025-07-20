405cac7e3435c4cdd1ff43299a030e67c3535919
ChazzitUP <hamzarezgui.education@gmail.com>
2025-07-20 09:58:51 +0100

    login api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 
@@ -11,7 +12,7 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.use("/api/admin", adminRouter);
 
 const PORT = process.env.PORT || 3000;
 
